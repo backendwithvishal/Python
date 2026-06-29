af4e2457bfb8bb09eeac0a13bb4a6f611e07c043
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-29 20:50:54 +0530

    code added in file

--- 2.Variables/Demo.py
+++ 2.Variables/Demo.py
@@ -1,11 +1,45 @@
 # variable is a container that stores value
 
-name = "Aryan"
-age = 12
-height = 4.5
-isMale = True
+# name = "Aryan"
+# age = 12
+# height = 4.5
+# isMale = True
 
-print(name)
-print(age)
-print(height)
-print(isMale)
+# print(name)
+# print(age)
+# print(height)
+# print(isMale)
+
+# age = 23
+
+# print(age)
+
+# #Good Variable Names
+# user_name = "john"
+# student = "Aryan"
+# _marks = 96
+
+# #Bad Variable Names
+# userName = "john"
+# Student = "Aryan"
+# _Marks = 96
+
+# a, b, c = 10, 20, 90
+
+# v, s, a = "Vishal", "Sanam", "Aryan"
+
+# print(v , s, a)
+
+# print(a, b, c)
+
+x = y = z = 50
+
+print(x, y, z)
+
+name = "Aryan"
+color = "Blue"
+age = 12
+
+print(f"My name is {name} and my favorite color is {color} " , "and my age is " , age)
+
+print("My name is "+  name + " and my favorite color is " + color + "and my age is "  , age)
