cc3c75b2066e826a591e2758f564b5b0ff77c417
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-30 20:35:39 +0530

    comment added in code base

--- 3.DataType/Demo.py
+++ 3.DataType/Demo.py
@@ -1,18 +1,38 @@
-x = "hello world"
+x = "hello world" #string
 
-x = 20
+x = 20 #integer
 
-x = -20
+x = -20 #integer
 
-x = 20.56
+x = 20.56 # float
 
-x = 3 + 5.6
+x = 3 + 5.6 # float
 
-x = 3 + 5j
+x = 3 + 5j #complex
 
-x = False
+x = False #boolean
 
-x = 89 >95
+x = 89 >95 #boolean
+
+x = [10,20,30,40,50] #list
+
+x = {10:"Hello",20:"World"} #dict
+
+x = (10,20,30,40,50) #tuple
+
+x = {10,20,30,40,50} #set
+
+x = frozenset({10,20,30,40,50}) #frozenset
+
+x = None #None
+
+x = ["vishal","shravani","sanam"] #list
+
+x = ("vishal","shravani","sanam") #tuple
+
+x = range(10) #range
+
+x = [45, 7, 93] #list
 
 print(x)
 print(x,type(x))
