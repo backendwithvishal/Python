25b0273cad7823fc65e878d0760fb1103c8a62c9
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-07-01 12:24:10 +0530

    file added with code

---
+++ 4.List&Tuple/Demo1.py
@@ -0,0 +1,10 @@
+# tuple
+fruits = ("Apple","Banana","Cherry","Mango","Orange","Mango")
+
+new_fruits = ("blueberry","strawberry")
+
+fruits += new_fruits
+print(fruits)
+
+# print(fruits[2:4])
+# print(type(fruits))
