1b2a68b5b6c61b0b5f1f45ed098ddf1461738eee
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-07-01 12:19:08 +0530

    small update in codebase

--- 4.List&Tuple/Demo.py
+++ 4.List&Tuple/Demo.py
@@ -1,11 +1,21 @@
 # list 
 
-fruits = ["Apple","Banana","Cherry","Mango","Orange"]
+# fruits = ["Apple","Banana","Cherry","Mango","Orange"]
 
-fruits[1] = "Blueberry"
-print(fruits)
+# number = [10,56,45,7]
+# print(number)
+
+# fruits[1] = "Blueberry"
+# print(fruits)
 # print(fruits[0])
 # print(fruits[-4])
 # print(fruits[-1])
 # print(fruits[1:4])
 
+data = ["vishal",44,95.7,"sanam"]
+name , age , marks , middlename = data
+# print(type(data))
+print("name: ", name)
+print("age: ", age)
+print("middlename: ", middlename)
+print("marks: ", marks)
