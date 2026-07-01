71db86fbdf02f036e141135c094adaacc498fe38
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-07-01 12:11:59 +0530

    small update in codebase

--- 4.List&Tuple/Demo.py
+++ 4.List&Tuple/Demo.py
@@ -1,7 +1,11 @@
 # list 
 
 fruits = ["Apple","Banana","Cherry","Mango","Orange"]
+
+fruits[1] = "Blueberry"
+print(fruits)
 # print(fruits[0])
 # print(fruits[-4])
 # print(fruits[-1])
-print(fruits[1:4])
+# print(fruits[1:4])
+
