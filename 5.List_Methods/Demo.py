37adc21c27caefbaf9f583d045698b3f64ec1af3
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-07-03 12:33:44 +0530

    new code added in file

--- 5.List_Methods/Demo.py
+++ 5.List_Methods/Demo.py
@@ -1,2 +1,12 @@
 # List Methods
 
+fruits = ["apple", "banana", "cherry"]
+
+# fruits.append("orange")
+# fruits.insert(1, "Mango")
+
+# fruits.remove("banana")
+# fruits.pop(2)
+# fruits.clear()
+
+print(fruits)
