2601deb0e5d951841be1a3285979f5e1ab4c8ecc
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-07-03 13:01:06 +0530

    small update in codebase

--- 5.List_Methods/Demo.py
+++ 5.List_Methods/Demo.py
@@ -1,6 +1,7 @@
 # List Methods
 
-fruits = ["apple", "banana", "cherry"]
+# fruits = ["apple", "orange", "banana", "cherry"]
+# veg = ["potato", "carrot" ]
 
 # fruits.append("orange")
 # fruits.insert(1, "Mango")
@@ -9,4 +10,34 @@ fruits = ["apple", "banana", "cherry"]
 # fruits.pop(2)
 # fruits.clear()
 
-print(fruits)
+# fruits.sort(reverse=True)
+# fruits.reverse()
+
+# x = fruits.copy()
+# x = list(fruits)
+
+# x = fruits[0:2]
+# fruits.extend(veg)
+
+# fruits = ["apple", "orange", "banana", "cherry"]
+# x = fruits.index("orange", 1)
+# x = fruits.count("cherry")
+
+# numbers = [45, 98, 122, 94]
+
+# x = max(numbers)
+# x = min(numbers)
+# x = len(numbers)
+
+# Tuple to list 
+# color = ("red", "green") 
+
+# color_list = list(color)
+# result = list(color)
+
+# Set to list
+numbers = {10, 50, 90}
+
+result = list(numbers)
+
+print(result)
