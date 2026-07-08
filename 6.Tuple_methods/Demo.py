562aaf8179fdc0a4c8c12a75a0f057c1c0016749
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-07-08 21:03:59 +0530

    small update in codebase

--- 6.Tuple_methods/Demo.py
+++ 6.Tuple_methods/Demo.py
@@ -1,7 +1,11 @@
-# fruits = ("apple", "blueberry", "cheerry", "apple")
-fruits = "apple", "blueberry", "cheerry", "apple"
+fruits = ("apple", "blueberry", "cherry", "apple")
+# fruits = "apple", "blueberry", "cheerry", "apple"
+# nested_tuple = ((10,20),(30,40),(50,60))
 
+x = fruits.index("cherry")
+print(x)
 
-
-print(fruits[0:4])
+# print(nested_tuple)
+# print(fruits[0:4])
+# print(fruits.count("apple"))
 # print(fruits.count("apple"))
