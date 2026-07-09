e29a4b4f0a6e1cac01bd7c95a77bbbb186a69592
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-07-09 13:04:29 +0530

    small update in codebase

--- 7.Dictionary/Demo.py
+++ 7.Dictionary/Demo.py
@@ -1,28 +1,40 @@
 # Dictionary
 
-# student = {
-#     "name": "Vishal",
-#     "age": 21,
-#     "city": "Mumbai",
-#     "marks": 96.48,
-#     "passed": True,
-#     "skills": ["Java", "Python", "Node.js"]
-# }
+student = {
+    "name": "Vishal",
+    "age": 21,
+    "city": "Mumbai",
+    "marks": 96.48,
+    "passed": True,
+    "skills": ["Java", "Python", "Node.js"]
+}
 
 # second type to create a dictionary
 # student = {}
-student = dict(
-    name = "Vishal",
-    age = 21
-)
+# student = dict(
+#     name = "Vishal",
+#     age = 21
+# )
 
 # student["gender"] = "male"
 # student["age"] = 22
 
-print(student)
 # print(student["skills"])
 # print(student["skills"][1])
 
 # print(type(student))
 # print(student["name"])
 # print(student)
+
+# students = {
+#     "Student1" : {
+#         "name": "Vishal",
+#         "age": 21
+#     },
+#     "Student2" : {
+#         "name": "Aryan",
+#         "age": 12
+#     }
+# }
+
+# print(students["Student2"]["name"])
