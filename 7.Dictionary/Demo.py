537a02b395fb53e3d1aa3ad6c1e5ae4b075e2027
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-07-09 12:56:50 +0530

    code added in file

--- 7.Dictionary/Demo.py
+++ 7.Dictionary/Demo.py
@@ -1,2 +1,28 @@
 # Dictionary
 
+# student = {
+#     "name": "Vishal",
+#     "age": 21,
+#     "city": "Mumbai",
+#     "marks": 96.48,
+#     "passed": True,
+#     "skills": ["Java", "Python", "Node.js"]
+# }
+
+# second type to create a dictionary
+# student = {}
+student = dict(
+    name = "Vishal",
+    age = 21
+)
+
+# student["gender"] = "male"
+# student["age"] = 22
+
+print(student)
+# print(student["skills"])
+# print(student["skills"][1])
+
+# print(type(student))
+# print(student["name"])
+# print(student)
