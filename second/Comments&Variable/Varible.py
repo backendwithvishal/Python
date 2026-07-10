a5b3f6976be1e89a607d8e7b3ccdbec1034089f7
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-07-10 23:59:35 +0530

    code added in new file

--- second/Comments&Variable/Varible.py
+++ second/Comments&Variable/Varible.py
@@ -1,1 +1,10 @@
 # Variable
+
+a = 10
+b = "Hello"
+print(a)
+print(b)
+
+#  we cannot add the special charaters in variable name
+#  we cannot add the number in variable name at the start
+#  we cannot add the space in variable name 
