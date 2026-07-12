21b1b7630574b03b2eab77c39cdb21ea2f9c6cb0
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-07-12 17:23:56 +0530

    small update in codebase

--- second/3String&TypeConversion/Demo.py
+++ second/3String&TypeConversion/Demo.py
@@ -8,3 +8,14 @@ print(a[-1],a[-4])
 # Slicing method
 print(a[2:1:-1])
 print(a[::-1]) '''
+
+''' a = "Hello how are you"
+
+# how
+print(a[6:9])
+
+# you
+print(a[14:18])
+
+# Hello
+print(a[0:5]) '''
