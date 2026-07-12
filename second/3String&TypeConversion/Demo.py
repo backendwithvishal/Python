779d8e38ec2fb654ba0955de850a6a32a8f5ac68
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-07-12 18:02:30 +0530

    small update in codebase

--- second/3String&TypeConversion/Demo.py
+++ second/3String&TypeConversion/Demo.py
@@ -20,6 +20,40 @@ print(a[14:18])
 # Hello
 print(a[0:5]) '''
 
+# a = 59
+# a = 96
+
+# print(a)
+
 # Type Conversion
+# a = "12"
+''' a = 12.89
+b = int(a)
 
+print(type(a))
+print(type(b)) '''
+# print(type(b))
 
+# float conversion
+''' a = "12.89"
+a = float(a)
+
+print(type(a)) '''
+# print(type(b))
+
+# String Conversion
+''' a = 62
+b = 56.90
+
+a = str(a)
+b = str(b)
+
+print(type(a))
+print(type(b)) '''
+
+# Boolean Conversion
+a = 89
+b = ''
+
+print(bool(a))
+print(bool(b))
