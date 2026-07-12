3804954bba3935708a4a2038f12f9fce3e44b2fc
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-07-12 17:02:46 +0530

    small update in codebase

--- second/2.DataType/Demo.py
+++ second/2.DataType/Demo.py
@@ -1,21 +1,29 @@
 # number datatype
-a = 89
-
+'''a = 89
 print(a)
-print(type(a))
+print(type(a))'''
 
 # Float datatype
-b = 22/7
-print(type(b))
+'''b = 22/7
+print(type(b)) '''
 
 # Compax datatype
-c = 4+4j
-print(type(c))
+'''c = 4+4j
+print(type(c))'''
 
 #String datatype
-d = "Aryan"
-print(type(d))
+'''d = "Aryan"
+print(type(d))'''
 
 #String datatype
-e = 'Aryan'
-print(type(e))
+'''e = 'Aryan'
+print(type(e)) '''
+
+#Boolean datatype
+f = True
+print(f)
+print(type(f))
+
+g = False
+print(g)
+print(type(g))
