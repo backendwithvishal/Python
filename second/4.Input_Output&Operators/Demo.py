2d915fccee2498759a405eff78fb81de8071c910
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-07-13 11:17:49 +0530

    new code added in file

--- second/4.Input_Output&Operators/Demo.py
+++ second/4.Input_Output&Operators/Demo.py
@@ -2,5 +2,8 @@
 
 name = "Vishal"
 age = 21
-print(f"Hii my name is {name} and I am {age} years old")
+# f-string 
+# print(f"Hii my name is {name} and I am {age} years old")
 
+# multiple arguments 
+print("Hii my name is", name, "and i am age is", age , "years old")
