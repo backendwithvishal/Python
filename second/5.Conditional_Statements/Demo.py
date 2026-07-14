8fda34dc16257f3f24f9a5de894e846bf1bbcdea
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-07-14 17:06:32 +0530

    more question added

--- second/5.Conditional_Statements/Demo.py
+++ second/5.Conditional_Statements/Demo.py
@@ -7,3 +7,52 @@ if age >= 18:
 else:
     print("you can't vote 🚫 go back to home") '''
 
+''' mom = int(input("Pls give me Momey: "))
+
+if mom == 100:
+    print("MCD Meal for you 🍔")
+    
+elif mom == 500:
+    print("5 Star Meal for you 🍕")
+    
+elif mom == 1000:
+    print("Pizza and Burger Meal for you 🍔🍕")
+    
+else :
+    print("No money No entry 🚫") '''
+
+# Question 1
+
+''' num1 = int(input (" pls 1 no is greater: "))
+num2 = int(input (" pls 2 no is greater: "))
+
+if num1 > num2:
+    print("1 no is greater: ")
+elif num2 > num1:
+    print("2 no is greater:")
+else:
+    print("both no is same") '''
+
+# Question 2
+
+# print("hello" > "Hello")
+
+# Question 3
+
+''' gen = input("pls enter your gender in M or F: ")
+
+if gen == "M":
+    print("Boy or Sigma Man")
+elif gen == "F":
+    print("Girl or Women")
+else:
+    print("Why are u gay?") '''
+
+# Question 4
+
+''' a = int(input("pls tell the no: "))
+
+if a % 2 == 0:
+    print("Even no")
+else:
+    print("Odd no") ''' 
