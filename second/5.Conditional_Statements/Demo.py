573d013842316b97c12cc2f0bc9aca7645600c03
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-07-14 16:45:13 +0530

    code added in new file

--- second/5.Conditional_Statements/Demo.py
+++ second/5.Conditional_Statements/Demo.py
@@ -1,1 +1,9 @@
-print("Hello World")
+# you have to take input of age and tell the person can vote or not
+
+''' age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("you can vote ✅")
+else:
+    print("you can't vote 🚫 go back to home") '''
+
