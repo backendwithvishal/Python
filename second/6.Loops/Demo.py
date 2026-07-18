1e0ac404b63096a9b7863623f0fbc5dd235d0862
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-07-18 20:19:09 +0530

    small update in codebase

--- second/6.Loops/Demo.py
+++ second/6.Loops/Demo.py
@@ -3,9 +3,9 @@
 # 23 - 56
 # 0 - 46
 
-range(10, 101, 1)
+''' range(10, 101, 1)
 range(23, 57, 1)
-range(0,47)
+range(0,47)'''
 
 '''for v in range(10,21,2):
     print(v) '''
@@ -13,7 +13,29 @@ range(0,47)
 # for v in range(5, 51, 5):
 #     print(v)
 
-v = int(input("Pls tell your number : "))
+''' v = int(input("Pls tell your number : "))
 
 for s in range(v,(v*10)+1,v):
-    print(s)
+    print(s) '''
+
+a = "Students"
+
+# for i in a:
+    # print(i)
+
+# for v in range(0,len(a)):
+#     print(f"{v} : {a[v]}")
+
+# for v in range(1, 11):
+#     if v == 4:
+#         break
+#     print(v)
+# else:
+#     print("Loop end 3")
+
+# for v in range(1, 11):
+#     if v == 4 or v == 6:
+#         continue
+#     print(v)
+# else:
+#     print("skip 4 & 6")
