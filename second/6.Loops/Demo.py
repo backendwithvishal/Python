830941da6c62599084da78ab65886ea41e52232d
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-07-18 19:55:01 +0530

    code added in file

--- second/6.Loops/Demo.py
+++ second/6.Loops/Demo.py
@@ -7,5 +7,13 @@ range(10, 101, 1)
 range(23, 57, 1)
 range(0,47)
 
-for v in range(10,21):
-    print(v)
+'''for v in range(10,21,2):
+    print(v) '''
+
+# for v in range(5, 51, 5):
+#     print(v)
+
+v = int(input("Pls tell your number : "))
+
+for s in range(v,(v*10)+1,v):
+    print(s)
