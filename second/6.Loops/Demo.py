20c87b780e4ef919bfcae1082bc5f8a9a1907665
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-07-20 17:19:26 +0530

    for loop question solved

--- second/6.Loops/Demo.py
+++ second/6.Loops/Demo.py
@@ -18,7 +18,7 @@ range(0,47)'''
 for s in range(v,(v*10)+1,v):
     print(s) '''
 
-a = "Students"
+# a = "Students"
 
 # for i in a:
     # print(i)
@@ -39,3 +39,33 @@ a = "Students"
 #     print(v)
 # else:
 #     print("skip 4 & 6")
+
+''' 1. n = int(input("tell me number: "))
+for v in range(n):
+    print("Hello World")
+
+2. n = int(input("tell me number: "))
+for v in range(1, n+1):
+    print(v) 
+
+3. n = int(input("Tell me your number: "))
+for v in range(n,0,-1):
+    print(v) 
+
+4. n = int(input("Which table u want to know: "))
+
+for v in range(1, 11):
+    print(f"{n} x {v} = {n*v}") '''
+
+# s = 0
+# v = int(input("tell me your no where u want sum: "))
+# for vs in range(1,v+1):
+#     s = s + vs
+
+s = 0
+v = int(input("Tell me the number up to which you want the sum: "))
+
+for vs in range(1, v + 1):
+    s = s + vs
+
+print("Sum =", s)
